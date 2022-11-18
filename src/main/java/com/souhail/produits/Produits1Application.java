895732a55bed4202d72289b1ffc04af3807f0ecb
souhailmihoubi <souhaiel.mihoubi@nabeul.r-iset.tn>
2022-11-18 12:28:34 +0100

    devoir3

---
+++ src/main/java/com/souhail/produits/Produits1Application.java
@@ -0,0 +1,27 @@
+package com.souhail.produits;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+
+import com.souhail.produits.entities.Produit;
+
+@SpringBootApplication
+public class Produits1Application implements CommandLineRunner {
+	
+	@Autowired
+	private RepositoryRestConfiguration repositoryRestConfiguration;
+
+	public static void main(String[] args) {
+		SpringApplication.run(Produits1Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		repositoryRestConfiguration.exposeIdsFor(Produit.class);
+	}
+
+}
---
+++ src/main/java/com/souhail/produits/entities/ProduitProjection.java
@@ -0,0 +1,10 @@
+package com.souhail.produits.entities;
+
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(name = "nomProd", types = { Produit.class })
+public interface ProduitProjection {
+	
+	public String getNomProduit();
+
+}
